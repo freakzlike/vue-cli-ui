9c60b758e641c647e00317d9d66bdb1fca482b4a
freakzlike <freakzlike@gmail.com>
2020-04-11 10:04:48 +0200

    Test commit

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -16,6 +16,26 @@
 </template>
 
 <script>
+  import {ServiceModel, fields} from 'vue-service-model'
+
+  class Post extends ServiceModel {
+    // Unique name to handle in vuex
+    static keyName = 'AppPost'
+
+    // Define service url
+    static urls = {
+      BASE: 'https://jsonplaceholder.typicode.com/posts/'
+    }
+
+    // Define field structure
+    static fieldsDef = {
+      id: new fields.Field(),
+      userId: new fields.Field(),
+      title: new fields.CharField({label: 'Title'}),
+      body: new fields.CharField({label: 'Body'})
+    }
+  }
+
   export default {
     name: 'MainView',
     data () {
@@ -23,8 +43,13 @@
         renderTime: this.$moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
       }
     },
+    created () {
+      this.sendRequest()
+    },
     methods: {
       async sendRequest () {
+        const post = await Post.objects.get('1')
+        console.log('post', post)
         const data = await this.$store.dispatch('environment/getData')
         console.log('data', data)
       }
